6343bff8c3f01eb39a7d34278e61aa932136a5b8
おこばやし <33394898+okobayashi@users.noreply.github.com>
2017-11-10 00:35:02 +0900

    Revert "Feature/chapter 3"

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,7 @@ var browserSync   = require('browser-sync');
 
 var paths = {
   "htmlSrc" : "./*.html",
-  "scssSrc" : "./src/scss/*.scss",
-  "imgSrc"  : "./src/images/**",
-  "rootDir" : "./dist/",
-  "imgDir"  : "./dist/images/",
 }
-
 
 gulp.task('bs', function() {
   browserSync.init({
@@ -22,33 +17,12 @@ gulp.task('bs', function() {
   });
 });
 
-gulp.task('scss', function() {
-  return gulp.src(paths.scssSrc)
-    .pipe($.sass()).on('error', $.sass.logError)
-    .pipe(gulp.dest(paths.rootDir + 'css'))
-    .pipe(browserSync.reload({
-      stream: true,
-      once  : true
-    }));
-});
-
 gulp.task('bs-reload', function() {
    browserSync.reload();
 });
 
-gulp.task('image', function() {
-  return gulp.src(paths.imgSrc)
-    .pipe(gulp.dest(paths.imgDir));
-});
-
-gulp.task('default', ['image', 'bs', 'scss', 'bs-reload'], function() {
+gulp.task('default', ['bs', 'bs-reload'], function() {
   $.watch([paths.htmlSrc],function(e) {
     gulp.start("bs-reload")
-  });
-  $.watch([paths.scssSrc],function(e) {
-    gulp.start("scss")
-  });
-  $.watch([paths.imgSrc],function(e) {
-    gulp.start("image")
   });
 });
